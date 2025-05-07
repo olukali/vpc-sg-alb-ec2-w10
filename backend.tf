61f616b9fc22f353eeab68ba62ee188d54eb1994
olu <woleajala2003@gmail.com>
2025-05-07 17:00:51 -0400

    bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "replace here with your bucket name"
+    bucket  = "oa-wk7-terraformstatebucket"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
